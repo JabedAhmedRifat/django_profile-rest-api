7bb3f8ff15f154c65195c11762515740ab05b4bf
Jabed Ahmed <jabedahmedrifat1@gmail.com>
2022-08-17 22:05:13 +0600

    added searching to user profile api

--- profile_api/views.py
+++ profile_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profile_api import serializers
 from profile_api import models
@@ -124,3 +125,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset= models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields= ('id','name', 'email',)
